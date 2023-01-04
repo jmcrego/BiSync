0968f7896946d48405bd40d37f1261fa788289b7
josep crego <josep.crego@systrangroup.com>
2023-01-04 17:53:52 +0100

    rightclick

--- bisync.js
+++ bisync.js
@@ -10,7 +10,6 @@ console_div = document.getElementById("console_div");
 console_table = document.getElementById("console_table");
 src_textarea = document.getElementById("src_textarea");
 tgt_textarea = document.getElementById("tgt_textarea");
-//sync_button = document.getElementById("sync_button");
 src_count = document.getElementById("src_count");
 tgt_count = document.getElementById("tgt_count");
 src_lang = document.getElementById("src_lang");
@@ -23,20 +22,7 @@ timeoutID = null;
 src_textarea_pred = '';
 tgt_textarea_pred = '';
 
-
-//hide/show debug console
-cbox_debug.addEventListener('change', (event) => {
-    if (cbox_debug.checked == true) {
-	console_div.removeAttribute("hidden");
-    }
-    else {
-	console_div.setAttribute("hidden", "hidden");
-    }
-});
-
-
-//when the initial HTML document has been completely loaded and parsed, without waiting for style sheets, images, and subframes to finish loading
-document.addEventListener("DOMContentLoaded", function(){
+function reset(){
     src_count.innerHTML = '0/' + textareaMaxLen;
     tgt_count.innerHTML = '0/' + textareaMaxLen;
     console_table.disabled = true;
@@ -46,44 +32,31 @@ document.addEventListener("DOMContentLoaded", function(){
     tgt_textarea.value = '';
     tag_s2t = par_op + 'to-' + tgt_lang.options[tgt_lang.selectedIndex].value + par_cl;
     tag_t2s = par_op + 'to-' + src_lang.options[src_lang.selectedIndex].value + par_cl;
-});
+    sync_time.value = 1;
+    sync_label.value = 1;
+}
+
+//hide menudiv when escape released
+document.addEventListener('keyup', (event) => {if (event.keyCode == 27){menudiv.setAttribute("hidden", "hidden");}});
+
+//when the initial HTML document has been completely loaded and parsed, without waiting for style sheets, images, and subframes to finish loading
+document.addEventListener("DOMContentLoaded", function(){reset();});
 
 //change of source language
-/*
-src_lang.addEventListener('change', (event) => {
-    tag_s2t = par_op + 'to-' + tgt_lang.options[tgt_lang.selectedIndex].value + par_cl;
-    tag_t2s = par_op + 'to-' + src_lang.options[src_lang.selectedIndex].value + par_cl;
-    src_textarea.value = ''
-    tgt_textarea.value = ''
-    src_count.innerHTML = '0/' + textareaMaxLen;
-    tgt_count.innerHTML = '0/' + textareaMaxLen;
-    console_table.disabled = true;
-    sync_button.disabled = true;
-    src_textarea.disabled = false;
-    tgt_textarea.disabled = false;
-});
-*/
+src_lang.addEventListener('change', (event) => {reset();});
 
 //change of target language
-/*
-tgt_lang.addEventListener('change', (event) => {
-    tag_s2t = par_op + 'to-' + tgt_lang.options[tgt_lang.selectedIndex].value + par_cl;
-    tag_t2s = par_op + 'to-' + src_lang.options[src_lang.selectedIndex].value + par_cl;
-    //initial situation
-    src_textarea.value = ''
-    tgt_textarea.value = ''
-    src_count.innerHTML = '0/' + textareaMaxLen;
-    tgt_count.innerHTML = '0/' + textareaMaxLen;
-    console_table.disabled = true;
-    sync_button.disabled = true;
-    src_textarea.disabled = false;
-    tgt_textarea.disabled = false;
+tgt_lang.addEventListener('change', (event) => {reset();});
+
+//hide/show debug console
+cbox_debug.addEventListener('change', (event) => {
+    if (cbox_debug.checked == true) {console_div.removeAttribute("hidden");}
+    else {console_div.setAttribute("hidden", "hidden");}
 });
-*/
 
 //when src_textarea is modified
 src_textarea.addEventListener('input', (event) => {
-    autoGrow(src_textarea,tgt_textarea);
+    //autoGrow(src_textarea,tgt_textarea);
     if (src_textarea.value.length){ //the other textarea is disabled
 	tgt_textarea.disabled = true;
 	src_textarea.value = clean_line(src_textarea.value);
@@ -99,7 +72,7 @@ src_textarea.addEventListener('input', (event) => {
 
 //when tgt_textarea is modified
 tgt_textarea.addEventListener('input', (event) => {
-    autoGrow(tgt_textarea,src_textarea);
+    //autoGrow(tgt_textarea,src_textarea);
     if (tgt_textarea.value.length){ //the other textarea is disabled
         src_textarea.disabled = true;
 	tgt_textarea.value = clean_line(tgt_textarea.value);
@@ -130,21 +103,21 @@ function autoGrow(oField,oField2) {
 
 function sync(){
     if (src_textarea.disabled){ //target-to-source
-	src_textarea.value = server_request_out(tgt_textarea.value, tag_t2s, src_textarea.value, src_textarea_pred); //updates src_textarea.value
+	src_textarea.value = server_request_sync(tgt_textarea.value, tag_t2s, src_textarea.value, src_textarea_pred); //updates src_textarea.value
 	src_textarea_pred = src_textarea.value;
 	src_textarea.disabled = false;
 	src_count.innerHTML = src_textarea.value.length + '/' + textareaMaxLen;
     }
     if (tgt_textarea.disabled){ //source-to-target
-	tgt_textarea.value = server_request_out(src_textarea.value, tag_s2t, tgt_textarea.value, tgt_textarea_pred); //updates tgt_textarea.value
+	tgt_textarea.value = server_request_sync(src_textarea.value, tag_s2t, tgt_textarea.value, tgt_textarea_pred); //updates tgt_textarea.value
 	tgt_textarea_pred = tgt_textarea.value;
 	tgt_textarea.disabled = false;
 	tgt_count.innerHTML = tgt_textarea.value.length + '/' + textareaMaxLen;
     }
 }
 
-src_textarea.addEventListener("contextmenu", function(e){e.preventDefault();}); //to avoid opening context menu (when right-clicking) in next function
 //when src_textarea right-clicked to prefix from a word
+src_textarea.addEventListener("contextmenu", function(e){e.preventDefault();}); //to avoid opening context menu (when right-clicking) in next function
 src_textarea.addEventListener('contextmenu', (event) => {
     if (src_textarea.value.length > 0 && tgt_textarea.value.length > 0 && src_textarea.selectionStart < src_textarea.value.length){ 
 	console.log('right-click on src_textarea pos='+src_textarea.selectionStart)
@@ -154,9 +127,8 @@ src_textarea.addEventListener('contextmenu', (event) => {
     }
 });
 
-
+//when tgt_textarea dblclicked to prefix from a word
 tgt_textarea.addEventListener("contextmenu", function(e){e.preventDefault();}); //to avoid opening context menu (when right-clicking) in next function
-//when tgt_textarea dblclicked (and shiftKey) to prefix from a word
 tgt_textarea.addEventListener('contextmenu', (event) => {
     if (tgt_textarea.value.length > 0 && src_textarea.value.length > 0 && tgt_textarea.selectionStart < tgt_textarea.value.length){ // && tgt_textarea.selectionEnd == tgt_textarea.selectionStart){
 	console.log('right-click on tgt_textarea pos='+tgt_textarea.selectionStart)
@@ -166,8 +138,8 @@ tgt_textarea.addEventListener('contextmenu', (event) => {
     }
 });
 
-//when src_textarea select (and not shiftKey) a gap to fill
-src_textarea.addEventListener('select', (event) => {
+//when src_textarea select (mouseup) a gap to fill
+src_textarea.addEventListener('mouseup', (event) => {
     if (src_textarea.value.length > 0 && tgt_textarea.value.length > 0 && src_textarea.selectionStart < src_textarea.value.length && src_textarea.selectionEnd > src_textarea.selectionStart){
 	console.log('select on src_textarea pos=['+src_textarea.selectionStart+','+src_textarea.selectionEnd+']')
 	txt = tgt_textarea.value + tag_t2s + src_textarea.value.substring(0,src_textarea.selectionStart) + '<GAP>' + src_textarea.value.substring(src_textarea.selectionEnd);
@@ -177,8 +149,8 @@ src_textarea.addEventListener('select', (event) => {
     }
 });
 
-//when tgt_textarea select (and not shiftKey) a gap to fill
-tgt_textarea.addEventListener('select', (event) => {
+//when tgt_textarea select (mouseup) a gap to fill
+tgt_textarea.addEventListener('mouseup', (event) => {
     if (src_textarea.value.length > 0 && tgt_textarea.value.length > 0 && tgt_textarea.selectionStart < tgt_textarea.value.length && tgt_textarea.selectionEnd > tgt_textarea.selectionStart){
 	console.log('select on tgt_textarea pos=['+tgt_textarea.selectionStart+','+tgt_textarea.selectionEnd+']')
 	txt = src_textarea.value + tag_s2t + tgt_textarea.value.substring(0,tgt_textarea.selectionStart) + '<GAP>' + tgt_textarea.value.substring(tgt_textarea.selectionEnd);
@@ -188,7 +160,7 @@ tgt_textarea.addEventListener('select', (event) => {
     }
 });
 
-//when one option is selected on the floating menu
+//when one option is selected on the floating menudiv
 menuselect.onchange = function(){
     menudiv.setAttribute("hidden", "hidden");
     console.log('selected ' + menuselect.value)
@@ -199,12 +171,12 @@ function showOptionsMenu(e,options){
     posY = e.clientY
     console.log('mouse on ' + posX + ', ' + posY)
     menudiv.style.left = posX + 'px';
-    menudiv.style.top  = posY + 'px';
+    menudiv.style.top  = posY+10 + 'px';
     menudiv.removeAttribute("hidden");
     return 'my new sentence';
 }
 
-async function server_request_out(src, tag, tgt, tgt_pred){
+async function server_request_sync(src, tag, tgt, tgt_pred){
     add_console_row(0, src + ' ' + tag + ' ' + tgt);
     params = { "src": src, "tgt": tgt, "tag": tag }
     console.log("REQUEST: "+JSON.stringify(params));
@@ -226,7 +198,7 @@ async function server_request_out(src, tag, tgt, tgt_pred){
 function add_console_row(pos, rest){
     row = console_table.insertRow(pos);
     cell = row.insertCell(0); //one single cell in row
-    if (pos == 0) {cell.style.backgroundColor = '#b3b3b3';}
+    if (pos == 0) {cell.style.backgroundColor = '#AAFFFF';}
     date = new Date(Date.now())
     mydate = date.toLocaleString('en-GB').replace(', ','-') + date.getMilliseconds()
     cell.innerHTML = '[' + mydate + '] ' + rest; //insert in console_table the input 
